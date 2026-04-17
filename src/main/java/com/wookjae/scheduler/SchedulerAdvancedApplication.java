51ef219f647e5ea8f5e86e65a53182733e48e6a9
WookJaes <dnrwo09@gmail.com>
2026-04-17 17:15:43 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/wookjae/scheduler/SchedulerAdvancedApplication.java
@@ -0,0 +1,13 @@
+package com.wookjae.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerAdvancedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerAdvancedApplication.class, args);
+    }
+
+}
